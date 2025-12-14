3aa1f8e9848c190aba2c77c04561624742ed0b61
imranmmd <mr.imranmammadov@gmail.com>
2025-12-14 17:50:08 +0000

    Task #0 completed

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""Defines a Square class."""
+
+
 class Square:
-    def _init__(self, size):
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size: The size of the square (no type or value validation).
+        """
         self.__size = size
+
